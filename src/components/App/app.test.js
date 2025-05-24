39495417ea0a87f3544324d851ac6420ac9101ea
Sai Krishna <saipotluri17@gmail.com>
2025-05-24 16:32:49 +0530

    Updated Folder Structure

--- src/components/App/app.test.js
+++ src/components/App/app.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './app';
+
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World!/i);
   expect(linkElement).toBeInTheDocument();
 });
